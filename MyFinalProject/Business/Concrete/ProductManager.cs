a4f4b61dc0369f7fae7f83401df3181cfd0c1fc0
GorkemPalali <grkmsrk.grsk@gmail.com>
2024-02-27 16:52:53 +0300

    MyFinalProject

---
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //....
+            //KOŞULLAR
+            //...
+
+
+            //Koşullar doğruysa aşağıdaki veriyi döndür
+           return _productDal.GetAll();
+        }
+    }
+}
